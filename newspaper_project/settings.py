6d4ae3f475341bac5456f81dcdc4a489ccfac8e1
agmorev <agmorev@gmail.com>
2019-05-23 10:23:30 +0300

    Add Heroku

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#p0jvmlph@2t1#eu097fr%!%18bmmdhvjl@%0*nnsjv!pf&m^%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
