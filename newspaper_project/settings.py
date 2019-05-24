35e2d6bc5e8d1ed6bb7b88e27aef783b25b4ea7d
agmorev <agmorev@gmail.com>
2019-05-24 15:46:30 +0300

    Some changes for Heroku

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -137,3 +138,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATE_INPUT_FORMATS = ("%d %b %Y", )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
